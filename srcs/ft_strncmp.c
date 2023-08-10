adfb9e274d4fd58af28bef10641fdb3f72a195a0
hadri96 <68434862+hadri96@users.noreply.github.com>
2023-08-10 17:09:20 +0200

    Norminette fixes + gitignore

--- srcs/ft_strncmp.c
+++ srcs/ft_strncmp.c
@@ -6,18 +6,18 @@
 /*   By: hmorand <hmorand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 16:49:18 by hmorand           #+#    #+#             */
-/*   Updated: 2023/08/10 17:05:46 by hmorand          ###   ########.fr       */
+/*   Updated: 2023/08/10 17:08:44 by hmorand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int ft_strncmp(char *s1, char *s2, size_t n)
+int	ft_strncmp(char *s1, char *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	while ((*s1 || *s2)  && i < n)
+	while ((*s1 || *s2) && i < n)
 	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
