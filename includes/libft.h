7e9a6179c2cdb45b71f4d6b2c796d0e16cdaebf9
hadri96 <68434862+hadri96@users.noreply.github.com>
2023-08-10 17:08:16 +0200

    Updated header file

--- includes/libft.h
+++ includes/libft.h
@@ -6,20 +6,26 @@
 /*   By: hmorand <hmorand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 11:20:23 by hmorand           #+#    #+#             */
-/*   Updated: 2023/08/10 16:13:10 by hmorand          ###   ########.fr       */
+/*   Updated: 2023/08/10 17:07:50 by hmorand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
+#include <stdlib.h>
 
 int		ft_strlen(char *str);
 int		ft_isalpha(char *str);
 int		ft_isdigit(char *str);
 int		ft_isprint(char *str);
+int		ft_strcmp(char *s1, char *s2);
+int 	ft_strncmp(char *s1, char *s2, size_t n);
 
-char	ft_toupper(int c);
+char 	ft_toupper(int c);
 char	ft_tolower(int c);
+char	*ft_strchr(char *str, int c);
+char 	*ft_strrchr(char *str, int c);
+
 
 char	*ft_itoa(int nb);
 int		ft_atoi(char *str);
