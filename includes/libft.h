13ac959eb022f41f4041828702ec5f85ce1f7b6a
hadri96 <68434862+hadri96@users.noreply.github.com>
2023-08-10 16:15:04 +0200

    Norminette fixes - header file

--- includes/libft.h
+++ includes/libft.h
@@ -6,22 +6,22 @@
 /*   By: hmorand <hmorand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 11:20:23 by hmorand           #+#    #+#             */
-/*   Updated: 2023/08/09 23:47:54 by hmorand          ###   ########.fr       */
+/*   Updated: 2023/08/10 16:13:10 by hmorand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int ft_strlen(char *str);
-int ft_isalpha(char *str);
-int ft_isdigit(char *str);
-int ft_isprint(char *str);
+int		ft_strlen(char *str);
+int		ft_isalpha(char *str);
+int		ft_isdigit(char *str);
+int		ft_isprint(char *str);
 
-char ft_toupper(int c);
-char ft_tolower(int c);
+char	ft_toupper(int c);
+char	ft_tolower(int c);
 
-char *ft_itoa(int nb);
-int ft_atoi(char *str);
+char	*ft_itoa(int nb);
+int		ft_atoi(char *str);
 
 #endif
