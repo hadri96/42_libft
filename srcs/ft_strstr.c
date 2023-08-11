0f5f2745d6ef0baec3a954066103acbc7f546397
hadri96 <68434862+hadri96@users.noreply.github.com>
2023-08-11 14:31:55 +0200

    Update of strstr tests

--- srcs/ft_strstr.c
+++ srcs/ft_strstr.c
@@ -6,43 +6,53 @@
 /*   By: hmorand <hmorand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 17:19:20 by hmorand           #+#    #+#             */
-/*   Updated: 2023/08/11 13:54:02 by hmorand          ###   ########.fr       */
+/*   Updated: 2023/08/11 14:31:05 by hmorand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strstr(const char *str, const char *substr)
+char	*ft_strstr(char *str, const char *substr)
 {
 	int	i;
-	int	j;
 
-	j = 0;
-	while (str[j])
+	while (*str)
 	{
 		i = 0;
-		while (str[i + j] == substr[i])
+		while (str[i] == substr[i])
 		{
 			if (!substr[i + 1])
-				return (str + j);
+				return (str);
 			i++;
 		}
-		j++;
+		str++;
 	}
 	return (NULL);
 }
 /*
 #include <stdio.h>
+#include <string.h>
 
 int	main(void)
 {
-	printf("Substring at beginning of string: %s\n",
+	printf("Homemade function - Substring at beginning of string: %s\n",
 		   ft_strstr("Hello world", "Hel"));
-	printf("Substring in the middle of string: %s\n",
+	printf("Standard function - Substring at beginning of string: %s\n",
+		   ft_strstr("Hello world", "Hel"));
+	printf("---------------------------------------------------------------\n");
+	printf("Homemade function - Substring in the middle of string: %s\n",
 		   ft_strstr("Hello world", "wor"));
-	printf("Substring at the end of string: %s\n",
+	printf("Standard function - Substring in the middle of string: %s\n",
+		   ft_strstr("Hello world", "wor"));
+	printf("------------------------------------------------------------\n");
+	printf("Homemade function - Substring at the end of string: %s\n",
 		   ft_strstr("Hello world", "ld"));
-	printf("Substring not in string: %s\n",
+	printf("Standard function - Substring at the end of string: %s\n",
+		   ft_strstr("Hello world", "ld"));
+	printf("------------------------------------------------------\n");
+	printf("Homemade function - Substring not in string: %s\n",
+		   ft_strstr("Hello world", "sandwich"));
+	printf("Standard function - Substring not in string: %s\n",
 		   ft_strstr("Hello world", "sandwich"));
 }
  */
