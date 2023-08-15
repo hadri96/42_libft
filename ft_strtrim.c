92ab3597779fc1f144a337877e6d4247b4e38a26
hadri96 <68434862+hadri96@users.noreply.github.com>
2023-08-15 21:22:58 +0200

    strtrim - mini fix

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: hmorand <hmorand@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 09:51:57 by hmorand           #+#    #+#             */
-/*   Updated: 2023/08/15 21:11:21 by hmorand          ###   ########.fr       */
+/*   Updated: 2023/08/15 21:18:35 by hmorand          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	end = ft_strlen(s1);
 	while (s1[start] && ft_strchr(set, s1[start]))
 		start++;
-	while (s1[end - 1] && ft_strrchr(set, s1[end - 1]) && end > start)
-		end--;
+	if (end > 0)
+	{
+		while (s1[end - 1] && ft_strrchr(set, s1[end - 1]) && end > start)
+			end--;
+	}
 	result = ft_substr(s1, start, end - start);
 	return (result);
 }
