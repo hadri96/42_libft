b4ffb2ed94c5996ad4ecadc25e7a2a85499d9aa7
hadri96 <68434862+hadri96@users.noreply.github.com>
2024-02-27 09:56:56 +0100

    arr_pop

--- arr_utils/arr_pop.c
+++ arr_utils/arr_pop.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: hmorand <hmorand@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/27 09:38:01 by hmorand           #+#    #+#             */
-/*   Updated: 2024/02/27 09:49:15 by hmorand          ###   ########.ch       */
+/*   Created: 2024/02/27 09:56:51 by hmorand           #+#    #+#             */
+/*   Updated: 2024/02/27 09:56:51 by hmorand          ###   ########.ch       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,3 +36,56 @@ char	**strarr_pop(char **arr, int index)
 	new_arr[i - 1] = NULL;
 	return (new_arr);
 }
+
+/* int main(void)
+{
+	char **arr = (char **)galloc(sizeof(char *) * 5);
+	arr[0] = ft_strdup("Hello");
+	arr[1] = ft_strdup("World");
+	arr[2] = ft_strdup("Copilot");
+	arr[3] = ft_strdup("GitHub");
+	arr[4] = ft_strdup("Assistant");
+
+	ft_printf("Original array:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		ft_printf("%s\n", arr[i]);
+	}
+
+	ft_printf("\n");
+
+	// Test normal case
+	int index = 2;
+	char **new_arr = strarr_pop(arr, index);
+	ft_printf("Array after removing element at index %d:\n", index);
+	for (int i = 0; i < 4; i++)
+	{
+		ft_printf("%s\n", new_arr[i]);
+	}
+
+	ft_printf("\n");
+
+	// Test edge case
+	index = 0;
+	new_arr = strarr_pop(arr, index);
+	ft_printf("Array after removing element at index %d:\n", index);
+	for (int i = 0; i < 3; i++)
+	{
+		ft_printf("%s\n", new_arr[i]);
+	}
+
+	ft_printf("\n");
+
+	// Test error case
+	index = 5;
+	new_arr = strarr_pop(arr, index);
+	if (new_arr == NULL)
+	{
+		ft_printf("Error: Index out of range\n");
+	}
+
+	free_strarr(arr);
+	free_strarr(new_arr);
+
+	return (0);
+} */
